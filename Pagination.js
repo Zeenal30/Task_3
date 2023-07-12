1eeb3b4820da952aa77e7b4c7b17e4fa0ea7588c
Zeenal <133235868+Zeenal30@users.noreply.github.com>
2023-07-12 22:02:05 +0530

    Update Pagination.js

--- Pagination.js
+++ Pagination.js
@@ -1,18 +1,18 @@
 let page = 0;
 var data = new Array();
-data.push(["Kena Doshi", "D22CE162", "9999955555","d22ce162@charusat.edu.in", "7.20 CGPA"]);
-data.push(["Zeenal Makati", "D22CE168", "8888855555","d22ce168@charusat.edu.in", "7.19 CGPA"]);
-data.push(["Parthvi Patel", "D22CE177", "9999989855","d22ce177@charusat.edu.in", "7.10 CGPA"]);
-data.push(["Het Tala", "D22CE157", "9109955555","d22ce157@charusat.edu.in", "7.13 CGPA"]);
-data.push(["Parth Shah", "21CE130", "9936555555","21ce130@charusat.edu.in", "7.09 CGPA"]);
-data.push(["Kathan Shah", "21CE126", "9999648555","21ce126@charusat.edu.in", "7.05 CGPA"]);
-data.push(["Manan Patel", "21CE096", "9965475555","21ce096@charusat.edu.in", "7.10 CGPA"]);
-data.push(["Nishil Patel", "21CE101", "9999933333","21ce101@charusat.edu.in", "7.11 CGPA"]);
-data.push(["Kalpit Patel", "21CE096", "9994444433","21ce096@charusat.edu.in", "7.06 CGPA"]);
-data.push(["Ramya Patel", "21CE183", "9999555553","21ce183@charusat.edu.in", "7.21 CGPA"]);
-data.push(["Dhruvi Makati", "21CE182", "9999944444","21ce182@charusat.edu.in", "7.19 CGPA"]);
-data.push(["Diya Patel", "21CE183", "9999666664","21ce0183@charusat.edu.in", "7.15 CGPA"]);
-data.push(["Manali Patel", "21CE184", "9999666664","21ce184@charusat.edu.in", "7.20 CGPA"]);
+data.push(["Kena Doshi", "D22CE162","d22ce162@charusat.edu.in", "7.20 CGPA"]);
+data.push(["Zeenal Makati", "D22CE168","d22ce168@charusat.edu.in", "7.19 CGPA"]);
+data.push(["Parthvi Patel", "D22CE177","d22ce177@charusat.edu.in", "7.10 CGPA"]);
+data.push(["Het Tala", "D22CE157","d22ce157@charusat.edu.in", "7.13 CGPA"]);
+data.push(["Parth Shah", "21CE130","21ce130@charusat.edu.in", "7.09 CGPA"]);
+data.push(["Kathan Shah", "21CE126","21ce126@charusat.edu.in", "7.05 CGPA"]);
+data.push(["Manan Patel", "21CE096","21ce096@charusat.edu.in", "7.10 CGPA"]);
+data.push(["Nishil Patel", "21CE101","21ce101@charusat.edu.in", "7.11 CGPA"]);
+data.push(["Kalpit Patel", "21CE096","21ce096@charusat.edu.in", "7.06 CGPA"]);
+data.push(["Ramya Patel", "21CE183","21ce183@charusat.edu.in", "7.21 CGPA"]);
+data.push(["Dhruvi Makati", "21CE182","21ce182@charusat.edu.in", "7.19 CGPA"]);
+data.push(["Diya Patel", "21CE183","21ce0183@charusat.edu.in", "7.15 CGPA"]);
+data.push(["Manali Patel", "21CE184","21ce184@charusat.edu.in", "7.20 CGPA"]);
 
 function load() {
   console.warn("loaded");
